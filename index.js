196d91df4dac938aa2831c06823100f20fa3d04f
elandau91 <eli.b.landau@gmail.com>
2020-08-03 17:07:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let texty = document.getElementById("text")
+    texty.textContent = "This is really cool!"
+  });
+   
+  
